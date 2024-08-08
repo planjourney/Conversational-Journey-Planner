503e44cb43a679aafd90bc47c109a33df40d5a26
planjourney <birsenyakut@gmail.com>
2024-08-08 13:37:15 +0100

    Update app.py
    Add feedback form

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import streamlit as st
 import backend
+import pandas as pd
+import datetime
 
 # Function to show or hide the information
 def toggle_info():
@@ -24,8 +23,6 @@ if "user_input_count" not in st.session_state:
 # Streamlit application setup
 st.title('Conversational Journey Planner for Londoners')
 
-
-
 # Display the information if the button is clicked
 if "show_info" in st.session_state and st.session_state["show_info"]:
     st.info(" Providing specific details, such as the exact name of a station "
@@ -38,12 +35,12 @@ st.button("Press for more info", on_click=toggle_info)
 # Display the conversation history
 for message in st.session_state.messages:
     if message["role"] == "user":
-        st.markdown(f"**You**: {message['content']}")
+        st.markdown(f"**User**: {message['content']}")
     else:
-        st.markdown(f"**TfL journey buddy**: {message['content']}")
+        st.markdown(f"**System**: {message['content']}")
 
 # Input text box for user query with a unique key
-user_query = st.text_input(f"You:", key=f"user_input_{st.session_state.user_input_count}")
+user_query = st.text_input(f"User:", key=f"user_input_{st.session_state.user_input_count}")
 
 # Button to submit the query
 if st.button("Send"):
@@ -68,5 +65,27 @@ if st.button("Send"):
         st.session_state.user_input_count += 1
 
         # Rerun to update the chat
-        st.rerun()
+        st.experimental_rerun()
+
+# Feedback Form in Sidebar
+st.sidebar.header("Feedback Form")
+st.sidebar.write("We would love to hear your feedback!")
+
+with st.sidebar.form(key='feedback_form'):
+
+    feedback = st.text_area("Your Feedback", placeholder="")
+    submit_button = st.form_submit_button(label='Submit')
+
+if submit_button:
+    feedback_data = {
+
+        "Feedback": feedback,
+        "Date": datetime.datetime.now()
+    }
+    # Save feedback data to a CSV file
+    df = pd.DataFrame([feedback_data])
+    df.to_csv('feedback.csv', mode='a', header=False, index=False)
+    st.sidebar.success("Thank you for your feedback!")
+
+
 
